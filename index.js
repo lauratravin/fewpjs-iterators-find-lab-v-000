370313bf57e2b347c67ac5598faa7539cbec9b7a
Laura Messa Travin <laura.travin@gmail.com>
2021-05-04 20:41:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function testFunc() {
 
 const superbowlWin = (aGames) => {
  let gameResult =  aGames.find( value  => value.result === "W"  )
- return !!gameResult ? result.year : undefined
+ return !!gameResult ? gameResult.year : undefined
 }
